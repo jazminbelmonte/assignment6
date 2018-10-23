9fc37bdb3b8ac76fddf43223032120b1c954afa4
jazminbelmonte <jazminbelmonte@gmail.com>
2018-10-23 09:59:18 -0600

    finished quadratic hashtable

--- include/hash_functions.h
+++ include/hash_functions.h
@@ -6,19 +6,24 @@ int divideHF(T e, int capacity){
   return e % capacity;
 }
 
-//TODO: mid-square
+/*//TODO: mid-square
+//which involves squaring the key and uses a number of digits from
+//the middle of the result to create a hash code
 int midSquareHF(T e, int capacity) {
 
 }
 
 //TODO: folding
+//which breaks the key into multiple parts and adds these parts together
+//in some convenient way of your choosing to create a hash code
 int foldingHF(T e, int capacity) {
 
 }
 
 //TODO: selecting-digits
+//which uses the second, third, and fifth digits of the key to create a hash code.
 int selectDgtHF(T e, int capacity) {
 
-}
+}*/
 
 #endif
