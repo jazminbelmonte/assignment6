99e4f67ed2269f7af568dfd1c7fda37079f7b967
jazminbelmonte <jazminbelmonte@gmail.com>
2018-10-23 22:11:01 -0600

    worked on hash functions

--- include/hash_functions.h
+++ include/hash_functions.h
@@ -1,29 +1,47 @@
-#ifndef _HASH_FUNCTION_H_
-#define _HASH_FUNCTION_H_
+#ifndef _HASH_FUNCTIONS_H_
+#define _HASH_FUNCTIONS_H_
+
+#include <string>
+#include <sstream>
 
 template <typename T>
+
 int divideHF(T e, int capacity){
   return e % capacity;
 }
 
-/*//TODO: mid-square
-//which involves squaring the key and uses a number of digits from
-//the middle of the result to create a hash code
+//mid-square hash function
 int midSquareHF(T e, int capacity) {
+   int x = e * e;
+   std::string strx = std::to_string(x);
+   std::string mid = strx.substr(2, 2);
+   int y = std::stoi(mid);
 
+   return y;
 }
 
-//TODO: folding
-//which breaks the key into multiple parts and adds these parts together
-//in some convenient way of your choosing to create a hash code
+//folding hash function
 int foldingHF(T e, int capacity) {
+  std::string x = std::to_string(e);
+  std::string first = x.substr(0, 2);
+  std::string second = x.substr(2, 2);
+  std::string third = x.substr(4, 2);
+  int total = std::stoi(first) + std::stoi(second) + std::stoi(third);
 
+  return total;
 }
 
-//TODO: selecting-digits
-//which uses the second, third, and fifth digits of the key to create a hash code.
+//selecting-digits hash function
 int selectDgtHF(T e, int capacity) {
+  std::string key;
+  std::string stre = std::to_string(e);
+  std::stringstream sin;
+  sin << stre.substr(1, 2);
+  sin << stre.substr(4, 1);
+  sin >> key;
+  int y = std::stoi(key);
 
-}*/
+  return y;
+}
 
 #endif
